aaac4cefbeaa4f7440efe349038bd665282bbb0e
Ahmed Bule <ahmedbule01@gmail.com>
2021-05-10 16:07:36 +0300

    added another promise

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ function calculate() {
 
     //fetch('items.json')   An example of get request
 
-    fetch('items.json').then(res => console.log(res));
+    fetch('items.json')
+        .then(res => res.json())
+        .then(data => console.log(data));
 
 
     // This runs asynchronslly, which means it runs in the background and it returns a promise. 
 
     // The way we catch a promise is .then
     // The .then will give us a response. 
+
+    // 200 status means it is working perfectly. 
+    //300 is redirect 
+    //400 is user error
+    //500 is server error 
 
 }
 
